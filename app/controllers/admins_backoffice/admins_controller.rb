eb5706449f5709386cc49e8caf8f1f6feedfca7e
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-08-10 03:40:49 -0300

    Create admin config

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -10,6 +10,19 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def edit
   end
 
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = @admin.new(params_admin)
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: "Adminstrador cadastrado"
+    else
+      render :new
+    end
+  end
+
   def update
     if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice: "Adminstrador atualizado"
