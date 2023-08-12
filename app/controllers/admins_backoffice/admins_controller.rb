cb2143f57ac298d8e8a229f91e40aa8da9ae7acd
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-08-12 19:58:59 -0300

    Destroy Route

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,7 +1,7 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
   
   before_action :verify_password, only: [:update]
-  before_action :set_admin, only: [:edit, :update]
+  before_action :set_admin, only: [:edit, :update, :destroy]
 
   def index
     @admins = Admin.all
@@ -28,6 +28,14 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
       redirect_to admins_backoffice_admins_path, notice: "Adminstrador atualizado"
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @admin.destroy
+      redirect_to admins_backoffice_admins_path, notice: "Adminstrador excluído"
+    else
+      render :index
     end
   end
 
