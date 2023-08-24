acdca5de88fcd64a9b1519669e5477047f93a821
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-08-23 21:41:48 -0300

    Layout Directions Corrections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     protected
 
         def layout_by_resource
-            if devise_controller? && resource_class === Admin
+            if devise_controller? && resource_class == Admin
                 "admin_devise"
             else
                 "application"
