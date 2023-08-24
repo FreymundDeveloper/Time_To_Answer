4e1d2b04270b15c18cf404efa26745305b264819
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-08-23 23:52:54 -0300

    Vagrant Web Console Config

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module TimeToAnswer
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    #Vagrant web onsole
+    #config.web_console.whitelisted_ips = '[IP]'
   end
 end
