5df47a898fd4248e721a6f24cc8367ef342e83c5
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-09-02 00:50:01 -0300

    Theme Search

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,12 @@
 module SiteHelper
+    def msg_jumbotron
+        case params[:action]
+        when 'index'
+            "Last questions registrated..."
+        when 'questions'
+            "Results to the search \"#{params[:term]}\"..."
+        when 'subject'
+            "Results to the theme \"#{params[:subject]}\"..."   
+        end 
+    end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -5,14 +5,20 @@ class Question < ApplicationRecord
 
   paginates_per 5
 
-  scope :_search_,  ->(page, term){
-    includes(:answers)
+  scope :_search_subject_, ->(page, subject_id){
+    includes(:answers, :subject)
+    .where(subject_id: subject_id)
+    .page(page)
+  }
+
+  scope :_search_, ->(page, term){
+    includes(:answers, :subject)
     .where("lower(description) LIKE ?", "%#{term.downcase}%")
     .page(page)
   }
 
   scope :last_question, ->(page){
-    includes(:answers).order('created_at desc').page(page)
+    includes(:answers, :subject).order('created_at desc').page(page)
   }
 
   #def self.last_question(page) # Model Method Exemple
