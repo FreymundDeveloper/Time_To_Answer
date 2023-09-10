f1755e4e9b8f1e4a5cca01fb77802965fa01fe85
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-09-10 05:23:10 -0300

    Admin Statistic Model

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,8 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :delete_all
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
+  after_create :set_statistic
+
   paginates_per 5
 
   scope :_search_subject_, ->(page, subject_id){
@@ -20,6 +22,12 @@ class Question < ApplicationRecord
   scope :last_question, ->(page){
     includes(:answers, :subject).order('created_at desc').page(page)
   }
+
+  private
+
+  def set_statistic
+    AdminStatistic.set_event(AdminStatistic::EVENTS[:total_questions])
+  end
 
   #def self.last_question(page) # Model Method Exemple
   # includes(:answers).order('created_at desc').page(page)
