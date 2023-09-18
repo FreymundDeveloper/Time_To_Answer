cb93362c442cbc2c79c383568fabf3d4df4026af
Lucas Freymund <lucasehresmann.osik@gmail.com>
2023-09-18 15:30:49 -0300

    Unicorn Task

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,7 +64,6 @@ set :keep_releases, 5
 after 'deploy:finished', 'deploy:restart'
 namespace :deploy do
     task :restart do
-        invoke 'unicorn:stop'
-        invoke 'unicorn:start'
+        invoke 'unicorn:restart'
     end
 end
